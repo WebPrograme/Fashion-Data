b09c4d384de3bf6426e23635c4b87928bc7c7af9
Yarne Gooris <yarne.gooris@gmail.com>
2022-12-20 20:07:04 +0100

    Updated

---
+++ split.py
@@ -0,0 +1,15 @@
+import pickle
+import requests
+import math
+
+# Load the image features
+img_features = pickle.load(open('image_features_embeddingWOMEN3.pkl', 'rb'))
+start = 0
+clutter_size = math.ceil(len(img_features)/66)
+
+for i in range(66):
+    pickle.dump(img_features[start:(i+1)*clutter_size], open('img_data\\files\\image_features_embeddingWOMEN{}.pkl'.format(i), 'wb'))
+
+    print(i)
+    
+    start = (i+1)*clutter_size
